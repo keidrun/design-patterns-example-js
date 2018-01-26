e5f6f83f1e33249072e413471f2a3a3776cc1b42
keidrun <kssk96@gmail.com>
2018-01-26 00:09:56 -0800

    Add composite pattern.

--- src/structural/composite.js
+++ src/structural/composite.js
@@ -1,20 +1,46 @@
-// function CompositeController(a) {
-//   this.a = a;
-// }
-// CompositeController.prototype.action = function(act) {
-//   let args = Array.prototype.slice.call(arguments);
-//   args.shift();
-//   for (let item in this.a) {
-//     this.a[item][act].apply(this.a[item], args);
-//   }
-// };
+// Objects
+class Order {
+  constructor() {}
+  addOrder(order) {}
+  amount() {}
+}
 
-// _cc = new CompositeController(_aCircle);
+class Item extends Order {
+  constructor(price) {
+    super();
+    this._price = price;
+  }
+  amount() {
+    return this._price;
+  }
+}
 
-// function tint(clr) {
-//   _cc.action('color', clr);
-// }
+class CompositeOrder extends Order {
+  constructor() {
+    super();
+    this._orders = [];
+  }
+  addOrder(order) {
+    this._orders.push(order);
+  }
+  amount() {
+    let total = 0;
+    this._orders.forEach(order => {
+      total += order.amount();
+    });
+    return total;
+  }
+}
 
-// function move(left, top) {
-//   _cc.action('move', left, top);
-// }
+// Test
+const item1 = new Item(1000);
+const item2 = new Item(100);
+const item3 = new Item(333.33);
+const order1 = new CompositeOrder();
+order1.addOrder(item1);
+order1.addOrder(item2);
+order1.addOrder(item3);
+console.log('item1 : $' + item1.amount());
+console.log('item2 : $' + item2.amount());
+console.log('item3 : $' + item3.amount());
+console.log('total : $' + order1.amount());
